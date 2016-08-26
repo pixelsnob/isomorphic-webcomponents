708db69ee9ce09f1b71be71d223a1cac0b5b2f29
Luis A. Echeverria <pixelsnob@gmail.com>
2016-08-26 00:10:12 -0700

    Delete HTMLElement.js

--- client/components/HTMLElement.js
+++
@@ -1,8 +0,0 @@
-
-if (typeof HTMLElement != 'function') {
-  let _HTMLElement = function() {};
-  _HTMLElement.prototype = HTMLElement.prototype;
-  HTMLElement = _HTMLElement;
-}
-
-export default HTMLElement;
