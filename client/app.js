26127bc2bd58b7276e2e4cb391c01da0948c01e8
Luis A. Echeverria <pixelsnob@gmail.com>
2016-08-28 15:49:24 -0700

    More view engine refactoring

--- client/app.js
+++ client/app.js
@@ -1,10 +1,10 @@
 
 import mutation_observer from 'webcomponents.js/dist/MutationObserver';
+//import sd from 'webcomponents.js/dist/ShadowDOM';
+import wc from 'webcomponents.js/dist/webcomponents-lite';
 import custom_elements from 'webcomponents.js/dist/CustomElementsV1.min';
-//import wc from 'webcomponents.js/dist/webcomponents';
 
 import TestTag from 'components/test_tag';
 
 window.customElements.define('test-tag', TestTag);
-
 
--- server/lib/view_engine.js
+++ server/lib/view_engine.js
@@ -5,31 +5,43 @@
  * scripts between client and server
  * 
  */
+import jsdom from 'jsdom';
 import path from 'path';
 import fs from 'fs';
-import getDomHtml from 'lib/get_dom_html';
 
 // These will appear as <script> tags
 let client_scripts = [
   'dist/client.js'
 ];
 
-// These will be evaluated when the window is created -- not visible
-// to client
+// Provide client script to jsdom as raw (ES5) src
 let src_scripts = client_scripts.map(script_path =>
   fs.readFileSync(path.resolve('public', script_path), 'utf8'));
-
-// Turn off client scripts for debugging
-//client_scripts = [];
 
 export default (file_path, opts, cb) => {
   fs.readFile(file_path, 'utf8', (err, html) => {
     if (err) {
       return cb(err);
     }
-    getDomHtml(html, src_scripts, client_scripts).then(html => {
-      cb(null, html);
-    }).catch(cb);
+    jsdom.env({
+      html,
+      src: src_scripts,
+      virtualConsole: jsdom.createVirtualConsole().sendTo(console),
+      done: (err, window) => {
+        if (err) {
+          return cb(err);
+        }
+        let { document } = window;
+        // Attach client scripts
+        let head_el = document.querySelector('head');
+        client_scripts.map(script => {
+          let script_el = document.createElement('script');
+          script_el.src = script;
+          head_el.appendChild(script_el);
+        });
+        cb(null, window.document.documentElement.outerHTML);
+      }
+    });
   });
 };
 
